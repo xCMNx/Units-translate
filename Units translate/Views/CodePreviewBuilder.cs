e5b35cf298424b0c8f6d1ad1c4ef78de20e1b47c
CriMsoN <DesktopxCMN@gmail.com>
2016-02-03 23:06:37 +0500

    Некое подобие оборачивания строки форматом, для возможности не только отрефакторить строку, но и сразу же обернуть её в метод

---
+++ Units translate/Views/CodePreviewBuilder.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Core;
+
+namespace Units_translate.Views
+{
+    static class CodePreviewBuilder
+    {
+        static Thickness lineNumPadding = new Thickness() { Left = 5, Right = 10 };
+        static SolidColorBrush numLineBrush = new SolidColorBrush(Color.FromArgb(30, 0, 0, 0));
+
+        static void AddLine(Grid grid, int number, string value, Brush fore, Brush numFore)
+        {
+            grid.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Auto });
+            var ctrl = new TextBlock() { Text = number.ToString(), Padding = lineNumPadding, Background = numLineBrush, Foreground = numFore };
+            grid.Children.Add(ctrl);
+            Grid.SetRow(ctrl, grid.RowDefinitions.Count - 1);
+            Grid.SetColumn(ctrl, 0);
+            ctrl = new TextBlock() { Text = value, Foreground = fore };
+            grid.Children.Add(ctrl);
+            Grid.SetRow(ctrl, grid.RowDefinitions.Count - 1);
+            Grid.SetColumn(ctrl, 1);
+        }
+
+        static void AddLine(StackPanel nums, StackPanel vals, int number, string value, Brush fore, Brush numFore)
+        {
+            nums.Children.Add(new TextBlock() { Text = number.ToString(), Padding = lineNumPadding, Background = numLineBrush, Foreground = numFore });
+            vals.Children.Add(new TextBlock() { Text = value, Foreground = fore });
+        }
+
+        public class PreviewMap
+        {
+            public readonly List<int> LineStart;
+            public readonly List<int> LineLength;
+            public readonly string Code;
+
+            static Regex linesRegex = new Regex(@"(?ixmn)(?<sl>^).*?(?<el>$)");
+            public PreviewMap(string code)
+            {
+                Code = code;
+                var matches = linesRegex.Matches(code);
+                LineStart = new List<int>(matches.Count);
+                LineLength = new List<int>(matches.Count);
+                foreach (Match m in matches)
+                {
+                    var idx = m.Groups["sl"].Index;
+                    LineStart.Add(idx);
+                    LineLength.Add(m.Groups["el"].Index - idx - 1);
+                }
+            }
+
+            public int LineIndexAt(int carretPos)
+            {
+                int line = LineStart.BinarySearch(carretPos);
+                if (line < 0)
+                    return ~line - 1;
+                return line;
+            }
+
+            public string LineAt(int line) => LineLength[line] > 0 ? Code.Substring(LineStart[line], LineLength[line]) : string.Empty;
+        }
+
+        public static Tuple<UIElement, Tuple<int, int, string>> MakePreview(PreviewMap map, IMapValueItem itm, int previwCount, string value)
+        {
+            var grid = new Grid() { VerticalAlignment = VerticalAlignment.Stretch };
+            Tuple<UIElement, Tuple<int, int, string>> res = new Tuple<UIElement, Tuple<int, int, string>>(grid, null);
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Auto });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() /*{ Width = GridLength.Auto }*/);
+            var nums = new StackPanel();
+            Grid.SetColumn(nums, 0);
+            var vals = new StackPanel();
+            var sb = new ScrollViewer() { Content = vals, HorizontalScrollBarVisibility = ScrollBarVisibility.Auto, VerticalScrollBarVisibility = ScrollBarVisibility.Disabled };
+            Grid.SetColumn(sb, 1);
+            grid.Children.Add(nums);
+            grid.Children.Add(sb);
+            var changed = !string.IsNullOrWhiteSpace(value);
+
+            var lIdx = map.LineIndexAt(itm.Start);
+            var lIdx2 = map.LineIndexAt(itm.End);
+            var lastLine = Math.Min(lIdx2 + previwCount, map.LineStart.Count - 1);
+            for (var line = Math.Max(lIdx - previwCount, 0); line <= lastLine; line++)
+            {
+                var brush = (line >= lIdx && line <= lIdx2) ? Brushes.Green : Brushes.Black;
+                AddLine(nums, vals, line + 1, map.LineAt(line), brush, brush);
+                if (changed && line == lIdx2)
+                {
+                    var rIdx = map.LineStart[lIdx];
+                    var rLen = map.LineStart[lIdx2] + map.LineLength[lIdx2] - map.LineStart[lIdx];
+                    string[] lines = map.Code.Substring(rIdx, rLen).
+                            Remove(itm.EditStart - map.LineStart[lIdx], itm.EditEnd - itm.EditStart).
+                            Insert(itm.EditStart - map.LineStart[lIdx], value).Replace("\r\n", "\n").Split('\n');
+                    if (lines.Length > 1)
+                    {
+                        var idx = lIdx;
+                        var tab = lines[0];
+                        //ищем отступ
+                        for (int i = 0; i < tab.Length; i++)
+                        {
+                            if (!char.IsWhiteSpace(tab[i]))
+                            {
+                                //оригинальный отступ + 2а пробела
+                                tab = tab.Substring(0, i) + "  ";
+                                break;
+                            }
+                        }
+                        //добавим отступы к строкам
+                        for (int i = 1; i < lines.Length; i++)
+                            lines[i] = tab + lines[i];
+                        //выведем строки в предпросмотр
+                        foreach (var ln in lines)
+                            AddLine(nums, vals, ++idx, ln, Brushes.Red, Brushes.Red);
+                    }
+                    else if (lines.Length == 1)
+                        AddLine(nums, vals, lIdx + 1, lines[0], Brushes.Red, Brushes.Red);
+                    //данные для замены в файле, индекс начала, длина, и текст на замену
+                    res = new Tuple<UIElement, Tuple<int, int, string>>(res.Item1, new Tuple<int, int, string>(rIdx, rLen, string.Join("\r\n", lines)));
+                }
+            }
+            return res;
+        }
+    }
+}
--- Units translate/Views/SampleView.xaml.cs
+++ Units translate/Views/SampleView.xaml.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using Core;
+using static Units_translate.Views.CodePreviewBuilder;
 
 namespace Units_translate.Views
 {
@@ -39,9 +38,9 @@ namespace Units_translate.Views
 
         public static readonly DependencyProperty NewValueProperty = DependencyProperty.Register(
           nameof(NewValue)
-          ,typeof(string)
-          ,typeof(SampleView)
-          ,new FrameworkPropertyMetadata(null, new PropertyChangedCallback(OnValueChanged))
+          , typeof(string)
+          , typeof(SampleView)
+          , new FrameworkPropertyMetadata(null, new PropertyChangedCallback(OnValueChanged))
         );
 
         public string NewValue
@@ -50,6 +49,40 @@ namespace Units_translate.Views
             set
             {
                 SetValue(NewValueProperty, value);
+                Update();
+            }
+        }
+
+        public static readonly DependencyProperty FormatProperty = DependencyProperty.Register(
+          nameof(Format)
+          , typeof(string)
+          , typeof(SampleView)
+          , new FrameworkPropertyMetadata("{0}", new PropertyChangedCallback(OnValueChanged))
+        );
+
+        public string Format
+        {
+            get { return (string)GetValue(FormatProperty); }
+            set
+            {
+                SetValue(FormatProperty, value);
+                Update();
+            }
+        }
+
+        public static readonly DependencyProperty UseFormatProperty = DependencyProperty.Register(
+          nameof(UseFormat)
+          , typeof(bool)
+          , typeof(SampleView)
+          , new FrameworkPropertyMetadata(false, new PropertyChangedCallback(OnValueChanged))
+        );
+
+        public bool UseFormat
+        {
+            get { return (bool)GetValue(UseFormatProperty); }
+            set
+            {
+                SetValue(UseFormatProperty, value);
                 Update();
             }
         }
@@ -80,31 +113,10 @@ namespace Units_translate.Views
 
         static Geometry apply = Geometry.Parse("F1 M 24.3042,23.9875L 31.2708,30.5582L 43.4624,16.6251L 46.7874,19.5542L 31.6667,38L 20.9792,27.3125L 24.3042,23.9875 Z M 16,17L 40.75,17L 38,20L 19,20L 19,40L 38,40L 38,33L 41,29.25L 41,43L 16,43L 16,17 Z M 51.854,35.8737L 59.7707,43.7903L 40.7708,62.7902L 32.8541,54.8736L 51.854,35.8737 Z M 61.2745,42.2067L 53.4374,34.3696L 56.7962,31.0108C 58.0329,29.7742 60.0379,29.7742 61.2746,31.0108L 64.6333,34.3696C 65.87,35.6062 65.87,37.6113 64.6333,38.8479L 61.2745,42.2067 Z M 30.875,65.5609L 30.0833,64.7693L 32.1614,58.1391L 37.5052,63.4829L 30.875,65.5609 Z");
         static Geometry show = Geometry.Parse("F1 M 38,33.1538C 40.6765,33.1538 42.8462,35.3235 42.8462,38C 42.8462,40.6765 40.6765,42.8461 38,42.8461C 35.3235,42.8461 33.1539,40.6765 33.1539,38C 33.1539,35.3235 35.3236,33.1538 38,33.1538 Z M 38,25.0769C 49.3077,25.0769 59,33.1538 59,38C 59,42.8461 49.3077,50.9231 38,50.9231C 26.6923,50.9231 17,42.8461 17,38C 17,33.1538 26.6923,25.0769 38,25.0769 Z M 38,29.1154C 33.0932,29.1154 29.1154,33.0932 29.1154,38C 29.1154,42.9068 33.0932,46.8846 38,46.8846C 42.9068,46.8846 46.8846,42.9068 46.8846,38C 46.8846,33.0932 42.9068,29.1154 38,29.1154 Z");
+        static Geometry wrap = Geometry.Parse("F1 M 37.3,50L 31.9943,50L 28.5748,40.8812C 28.447,40.5437 28.3134,39.9188 28.174,39.0062L 28.1174,39.0062L 27.66,40.9625L 24.2275,50L 18.9,50L 25.225,36L 19.4576,22L 24.8896,22L 27.721,30.3938C 27.9417,31.0604 28.1392,31.8479 28.3134,32.7563L 28.3701,32.7563L 28.9843,30.3188L 32.1032,22L 37.0212,22L 31.1797,35.8813L 37.3,50 Z M 44.5466,58.0533L 42.7733,58.0533C 40.24,55.1321 38.9733,51.53 38.9733,47.2471C 38.9733,42.9483 40.24,39.2882 42.7733,36.2667L 44.5466,36.2667C 42.0133,39.4017 40.7467,43.0539 40.7467,47.2233C 40.7467,51.3558 42.0133,54.9658 44.5466,58.0533 Z M 59.7267,58.0533L 57.9533,58.0533C 60.4867,54.9658 61.7533,51.3558 61.7533,47.2233C 61.7533,43.0539 60.4867,39.4017 57.9533,36.2667L 59.7267,36.2667C 62.26,39.2882 63.5267,42.9483 63.5267,47.2471C 63.5267,51.53 62.26,55.1321 59.7267,58.0533 Z M 58.84,39.3333L 53.7298,53.1083C 52.5106,56.4228 50.6753,58.08 48.2237,58.08C 47.2896,58.08 46.5217,57.9111 45.92,57.5733L 45.92,54.5333C 46.4267,54.8711 46.9769,55.04 47.5706,55.04C 48.5496,55.04 49.2318,54.5637 49.6171,53.611L 50.2662,52.0317L 45.16,39.3333L 49.4865,39.3333L 51.8179,47.0838C 51.963,47.5667 52.0765,48.1354 52.1583,48.7898L 52.2058,48.7898L 52.6096,47.1115L 54.9648,39.3333L 58.84,39.3333 Z");
 
-        static Regex linesRegex = new Regex(@"(?ixmn)(?<sl>^).*?(?<el>$)");
         static Thickness expanderMargin = new Thickness(5);
-        static Thickness lineNumPadding = new Thickness() { Left = 5, Right = 10 };
         static Thickness stackMargin = new Thickness() { Left = 10 };
-        static SolidColorBrush numLineBrush = new SolidColorBrush(Color.FromArgb(30, 0, 0, 0));
-
-        void AddLine(Grid grid, int number, string value, Brush fore, Brush numFore)
-        {
-            grid.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Auto });
-            var ctrl = new TextBlock() { Text = number.ToString(), Padding = lineNumPadding, Background = numLineBrush, Foreground = numFore };
-            grid.Children.Add(ctrl);
-            Grid.SetRow(ctrl, grid.RowDefinitions.Count - 1);
-            Grid.SetColumn(ctrl, 0);
-            ctrl = new TextBlock() { Text = value, Foreground = fore };
-            grid.Children.Add(ctrl);
-            Grid.SetRow(ctrl, grid.RowDefinitions.Count - 1);
-            Grid.SetColumn(ctrl, 1);
-        }
-
-        void AddLine(StackPanel nums, StackPanel vals, int number, string value, Brush fore, Brush numFore)
-        {
-            nums.Children.Add(new TextBlock() { Text = number.ToString(), Padding = lineNumPadding, Background = numLineBrush, Foreground = numFore });
-            vals.Children.Add(new TextBlock() { Text = value, Foreground = fore });
-        }
 
         void Update()
         {
@@ -113,108 +125,58 @@ namespace Units_translate.Views
             if (val == null || Data == null)
                 return;
             bool noChanges = val.Value == NewValue;
-            string code = Data.Text;
-            var previwCount = MainVM.Instance.PreviewLines / 2;
-
-            var matches = linesRegex.Matches(code);
-            var ls = new List<int>(matches.Count);
-            var ll = new List<int>(matches.Count);
-            foreach (Match m in matches)
+            bool filterEmpty = true;
+            if (UseFormat)
             {
-                var idx = m.Groups["sl"].Index;
-                ls.Add(idx);
-                ll.Add(m.Groups["el"].Index - idx - 1);
+                try
+                {
+                    filterEmpty = string.IsNullOrWhiteSpace(string.Format(Format, string.Empty));
+                }
+                catch { }
             }
+            var previwCount = MainVM.Instance.PreviewLines / 2;
+            var map = new PreviewMap(Data.Text);
 
             foreach (var item in Data.Items)
             {
                 var itm = item as IMapValueItem;
-                if (itm != null)
+                if (itm != null && itm.Value == val.Value)
                 {
-                    Tuple<int, int, string> itemdata = null;
-                    if (itm.Value == val.Value)
+                    string nVal = string.Empty;
+                    try
                     {
-                        var grid = new Grid() { VerticalAlignment = VerticalAlignment.Stretch };
-                        grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Auto });
-                        grid.ColumnDefinitions.Add(new ColumnDefinition() /*{ Width = GridLength.Auto }*/);
-                        var nums = new StackPanel();
-                        Grid.SetColumn(nums, 0);
-                        var vals = new StackPanel();
-                        var sb = new ScrollViewer() { Content = vals, HorizontalScrollBarVisibility = ScrollBarVisibility.Auto, VerticalScrollBarVisibility = ScrollBarVisibility.Disabled };
-                        Grid.SetColumn(sb, 1);
-                        grid.Children.Add(nums);
-                        grid.Children.Add(sb);
-
-                        var lIdx = ls.BinarySearch(itm.Start);
-                        var lIdx2 = ls.BinarySearch(itm.End);
-                        if (lIdx < 0) lIdx = ~lIdx - 1;
-                        if (lIdx2 < 0) lIdx2 = ~lIdx2 - 1;
-                        var lastLine = Math.Min(lIdx2 + previwCount, ls.Count - 1);
-                        for (var line = Math.Max(lIdx - previwCount, 0); line <= lastLine; line++)
-                        {
-                            var brush = (line >= lIdx && line <= lIdx2) ? Brushes.Green : Brushes.Black;
-                            AddLine(nums, vals, line + 1, ll[line] > 0 ? code.Substring(ls[line], ll[line]) : string.Empty, brush, brush);
-                            if (!noChanges && line == lIdx2)
-                            {
-                                var rIdx = ls[lIdx];
-                                var rLen = ls[lIdx2] + ll[lIdx2] - ls[lIdx];
-                                string[] lines = code.Substring(rIdx, rLen).
-                                        Remove(itm.EditStart - ls[lIdx], itm.EditEnd - itm.EditStart).
-                                        Insert(itm.EditStart - ls[lIdx], itm.NewValue(NewValue)).Replace("\r\n", "\n").Split('\n');
-                                if (lines.Length > 1)
-                                {
-                                    var idx = lIdx;
-                                    var tab = lines[0];
-                                    //ищем отступ
-                                    for (int i = 0; i < tab.Length; i++)
-                                    {
-                                        if (!char.IsWhiteSpace(tab[i]))
-                                        {
-                                            //оригинальный отступ + 2а пробела
-                                            tab = tab.Substring(0, i) + "  ";
-                                            break;
-                                        }
-                                    }
-                                    //добавим отступы к строкам
-                                    for (int i = 1; i < lines.Length; i++)
-                                        lines[i] = tab + lines[i];
-                                    //выведем строки в предпросмотр
-                                    foreach (var ln in lines)
-                                        AddLine(nums, vals, ++idx, ln, Brushes.Red, Brushes.Red);
-                                }
-                                else if (lines.Length == 1)
-                                    AddLine(nums, vals, lIdx + 1, lines[0], Brushes.Red, Brushes.Red);
-                                //данные для замены в файле, индекс начала, длина, и текст на замену
-                                itemdata = new Tuple<int, int, string>(rIdx, rLen, string.Join("\r\n", lines));
-                            }
-                        }
-
-                        var stack = new StackPanel() { HorizontalAlignment = HorizontalAlignment.Right, Orientation = Orientation.Horizontal };
-                        if (!noChanges)
-                        {
-                            var btnAdd = new Button() { Style = addBtn, DataContext = itemdata, Content = new System.Windows.Shapes.Path() { Data = apply, Fill = Brushes.Black, Stretch = Stretch.Uniform } };
-                            btnAdd.Click += Btn_Click;
-                            stack.Children.Add(btnAdd);
-                        }
-                        var btnShow = new Button() { Style = showBtn, DataContext = itm, Content = new System.Windows.Shapes.Path() { Data = show, Fill = Brushes.Black, Stretch = Stretch.Uniform } };
-                        btnShow.Click += BtnShow_Click;
-                        stack.Children.Add(btnShow);
-                        var header = lIdx == lIdx2 ? string.Format("line: {0}", lIdx + 1) : string.Format("lines: {0} - {1}", lIdx + 1, lIdx2 + 1);
-                        stack.Children.Add(new TextBlock()
-                        {
-                            Text = header,
-                            VerticalAlignment = VerticalAlignment.Center,
-                            Margin = stackMargin
-                        });
-                        container.Children.Add(new Expander()
-                        {
-                            Header = stack,
-                            Content = grid,
-                            IsExpanded = MainVM.Instance.ExpandedPreviews,
-                            Background = Brushes.LightYellow,
-                            Margin = expanderMargin
-                        });
+                        nVal = string.Format(UseFormat ? Format : "{0}", noChanges ? UseFormat ? itm.NewValue(itm.Value) : string.Empty : itm.NewValue(NewValue));
                     }
+                    catch { }
+                    var previewData = MakePreview(map, itm, previwCount, nVal);
+                    var stack = new StackPanel() { HorizontalAlignment = HorizontalAlignment.Right, Orientation = Orientation.Horizontal };
+                    if (!noChanges || !filterEmpty)
+                    {
+                        var btnAdd = new Button() { Style = addBtn, DataContext = previewData.Item2, Content = new System.Windows.Shapes.Path() { Data = apply, Fill = Brushes.Black, Stretch = Stretch.Uniform } };
+                        btnAdd.Click += Btn_Click;
+                        stack.Children.Add(btnAdd);
+                    }
+                    var btnShow = new Button() { Style = showBtn, DataContext = itm, Content = new System.Windows.Shapes.Path() { Data = show, Fill = Brushes.Black, Stretch = Stretch.Uniform } };
+                    btnShow.Click += BtnShow_Click;
+                    stack.Children.Add(btnShow);
+
+                    var lIdx = map.LineIndexAt(itm.Start);
+                    var lIdx2 = map.LineIndexAt(itm.End);
+                    var header = lIdx == lIdx2 ? string.Format("line: {0}", lIdx + 1) : string.Format("lines: {0} - {1}", lIdx + 1, lIdx2 + 1);
+                    stack.Children.Add(new TextBlock()
+                    {
+                        Text = header,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        Margin = stackMargin
+                    });
+                    container.Children.Add(new Expander()
+                    {
+                        Header = stack,
+                        Content = previewData.Item1,
+                        IsExpanded = MainVM.Instance.ExpandedPreviews,
+                        Background = Brushes.LightYellow,
+                        Margin = expanderMargin
+                    });
                 }
             }
         }
